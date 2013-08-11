c13c47dfe3e5ef6075853aa928e1157ae392df21
EJ Brennan <ejbrennan@gmail.com>
2013-08-11 18:26:27 -0400

    completed chapter on errors

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -13,10 +13,16 @@ Template.postSubmit.events({
 		// Meteor.Router.to('postPage', post);
 
 		Meteor.call('post', post, function(error, id) {
-			if(error) 
-				return alert(error.reason)
-
-			Meteor.Router.to('postPage', id);
+			if(error) {
+				// display the error to the user
+				throwError(error.reason);
+				// if the error is that the post already exists, take us there
+				if(error.error === 302) 
+					Meteor.Router.to('postPage', error.details);
+			}
+			else {
+				Meteor.Router.to('postPage', id);
+			}
 		});
 	}
 });
